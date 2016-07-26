2a4b1b6cfb9d62016eb0597b93de97996179047b
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-07-25 22:30:16 -0500

    complete simple working version

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require('path');
+
+
+
+
+router.get('/', function(req,res,next){
+    res.sendFile(path.join(__dirname, '../public/views/index.html'));
+});
+
+
+
+module.exports = router;
